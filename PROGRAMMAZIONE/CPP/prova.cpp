f71337dc181a699eea84c670ee1da3f5f00fc9e6
gio83dj <giorgio.leggio@gmail.com>
2018-01-26 11:48:11 +0100

    new file:   PROGRAMMAZIONE/CPP/matrices.cpp 	new file:   PROGRAMMAZIONE/CPP/media.cpp 	new file:   PROGRAMMAZIONE/CPP/numeroprimo.cpp 	new file:   PROGRAMMAZIONE/CPP/prova.cpp 	new file:   PROGRAMMAZIONE/EXE/matrices 	new file:   PROGRAMMAZIONE/EXE/media 	new file:   PROGRAMMAZIONE/EXE/primo 	new file:   PROGRAMMAZIONE/EXE/prova

---
+++ PROGRAMMAZIONE/CPP/prova.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long numero1;
+	long numero2;
+	cout << "Digita un numero";
+	cin >> numero1;
+	cout << "Digita un altro numero";
+	cin >> numero2;
+	cout << "Hai inserito " << numero1 << " e poi " << numero2;
+	return 0;
+}
